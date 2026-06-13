5a382d6b73d40b2d8ff329faece0d013545dcf35
Sunday <sunnexajayi@gmail.com>
2026-06-13 12:25:53 +0200

    feat: integrate whitenoise for static file serving in production

--- core/settings.py
+++ core/settings.py
@@ -68,6 +68,7 @@ AUTH_USER_MODEL = 'accounts.User'   # or wherever your User model lives
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -254,6 +255,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
